4cdf5f308f9d6ac7c95a4e119df5dbf865cb9df6
Tove Adamsson <toveadamsson@gmail.com>
2021-11-17 12:30:13 +0100

    clone try

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ function Intro() {
   return (
     <div className="home-page">
       <div className="introduction">
-        <h2>Welcome !</h2>
+        <h2>Welcome!</h2>
         <p>
           Quietly thriving is all about taking the power into your own hands –
           all in your pace. No competition, no need for attention, no need to
@@ -13,47 +13,42 @@ function Intro() {
       </div>
       <div className="contact">
         <div className="text">
-          <div className="tekst">
-          <p>
-            Are you feeling stuck in your career/ holding yourself back because
-            of your more quiet nature? Or are you struggling with socializing or
-            about to start your own business, but you’re feeling too
-            overwhelmed?</p>
+          <div>
             <p>
-
-            Book a first 30 minutes free call.
+              Are you feeling stuck in your career/ holding yourself back
+              because of your more quiet nature? Or are you struggling with
+              socializing or about to start your own business, but you’re
+              feeling too overwhelmed?
             </p>
-          
+            <p>Book a first 30 minutes free call.</p>
           </div>
-          <div className="button1">
+          <div className="button">
             <a
               href="https://calendly.com/nataschavejenhenriksen/30min?month=2021-03"
-              className="button-calendly"
+              className="button"
             >
               Book a meeting
             </a>
           </div>
         </div>
-        <div className="text2">
-          <div className="tekst">
-
-          <p>
-            Join the community on Facebook. A safe space for like-minded women.
-            Introverted women who are on a journey of personal growth.
-          </p>
-          <p className="text2-points">
-            - Weekly Check-ins/ Debates <br />- Monthly Workshops <br />- Full
-            support / Encouragement
-          </p>
+        <div className="text">
+          <div>
+            <p>
+              Join the community on Facebook. A safe space for like-minded
+              women. Introverted women who are on a journey of personal growth.
+            </p>
+            <p className="text2-points">
+              - Weekly Check-ins/ Debates <br />- Monthly Workshops <br />- Full
+              support / Encouragement
+            </p>
           </div>
-          <div className="button2">
-
-          <a
-            href="https://www.facebook.com/groups/quietlythriving"
-            className="button-facebook"
-          >
-            Go to Facebook
-          </a>
+          <div className="button">
+            <a
+              href="https://www.facebook.com/groups/quietlythriving"
+              className="button"
+            >
+              Go to Facebook
+            </a>
           </div>
         </div>
       </div>
